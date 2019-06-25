0d5144da9e946b391d8f9bb16081e2fb8bbf2020
andrew carr <drew4555@yahoo.com>
2019-06-25 13:11:26 -0500

    initial commit

---
+++ Domain/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Domain
+{
+    class Client
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string FirstName { get; set; }
+        [ForeignKey("ApplicationUser")]
+        public string applicationUserId { get; set; }
+        //public virtual ApplicationUser ApplicationUser { get; set; }
+    }
+}
